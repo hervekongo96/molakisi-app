8bded3ffb67138cea9bb06b7fa6eecc1d8c75eac
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-25 12:08:23 +0100

    database updated

--- db/molakisi.sql
+++ db/molakisi.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.4deb1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : localhost:3306
--- Généré le : ven. 21 juil. 2023 à 19:19
--- Version du serveur : 8.0.33-0ubuntu0.22.10.2
--- Version de PHP : 8.1.7-1ubuntu3.5
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 25, 2023 at 11:06 AM
+-- Server version: 8.0.31
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,17 +18,18 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `molakisi`
+-- Database: `molakisi`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `candidature`
+-- Table structure for table `candidature`
 --
 
-CREATE TABLE `candidature` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `candidature`;
+CREATE TABLE IF NOT EXISTS `candidature` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `postnom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
@@ -40,11 +41,12 @@ CREATE TABLE `candidature` (
   `file` varchar(100) NOT NULL,
   `cours` varchar(50) NOT NULL,
   `ecole` varchar(50) NOT NULL,
-  `photo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `photo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `candidature`
+-- Dumping data for table `candidature`
 --
 
 INSERT INTO `candidature` (`id`, `nom`, `postnom`, `prenom`, `sexe`, `email`, `phone`, `montant`, `anneExp`, `file`, `cours`, `ecole`, `photo`) VALUES
@@ -53,19 +55,52 @@ INSERT INTO `candidature` (`id`, `nom`, `postnom`, `prenom`, `sexe`, `email`, `p
 -- --------------------------------------------------------
 
 --
--- Structure de la table `tuteur`
+-- Table structure for table `registereleve`
 --
 
-CREATE TABLE `tuteur` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `registereleve`;
+CREATE TABLE IF NOT EXISTS `registereleve` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `postnom` varchar(50) NOT NULL,
+  `sexe` varchar(12) NOT NULL,
+  `naissance` varchar(50) NOT NULL,
+  `classe` varchar(50) NOT NULL,
+  `sujet` varchar(100) NOT NULL,
+  `objectif` varchar(100) NOT NULL,
+  `adresse` varchar(100) NOT NULL,
+  `tuters` varchar(50) NOT NULL,
+  `email` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `phoneP` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `registereleve`
+--
+
+INSERT INTO `registereleve` (`id`, `nom`, `prenom`, `postnom`, `sexe`, `naissance`, `classe`, `sujet`, `objectif`, `adresse`, `tuters`, `email`, `phoneP`) VALUES
+(1, 'j', 'j', 'j', 'h', '1112-11-11', 'k', 'j', 'k', 'kk', 'nn', 'herve@gmail.com', '232233');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tuteur`
+--
+
+DROP TABLE IF EXISTS `tuteur`;
+CREATE TABLE IF NOT EXISTS `tuteur` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `tuteur`
+-- Dumping data for table `tuteur`
 --
 
 INSERT INTO `tuteur` (`id`, `name`, `prenom`, `email`, `password`) VALUES
@@ -78,95 +113,44 @@ INSERT INTO `tuteur` (`id`, `name`, `prenom`, `email`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `password` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `prenom`, `email`, `password`) VALUES
 (1, 'Masala', 'Huram', 'molakisibusinessgrouo@gmail.com', '$2a$08$TEEHRiFzecCl2Z2ZLKbNCOe2pB1F3FiDNVFxkNbfFFMC97h82BO9O'),
 (2, 'masambukidi', 'samuela', 'samuela@gmail.com', '$2a$08$76RIc2OoL2vV157aSVZvuuB0UozDUCtjHc2/R3PO3pxHP6BQ07smq'),
 (3, 'okito', 'micheline', 'okitobibi@gmail.com', '$2a$08$qEHkOh51EAK/.vYcO/t3XO.sK2Akbp4iG411Fh28dMO62tTzu2cjW'),
-(4, 'simeon', 'tambu', 'si@gm', '$2a$08$PIiyq/U13ClLlVyjEp.OLedn7D4DrwDV68VMJQUHzivxwbOiOrvX6');
+(4, 'simeon', 'tambu', 'si@gm', '$2a$08$PIiyq/U13ClLlVyjEp.OLedn7D4DrwDV68VMJQUHzivxwbOiOrvX6'),
+(5, 'tam', 'sim', 'rubuzleonardo@gmail.com', '$2a$08$g2f5OLFdy/h3/jCo0E0NeuQjDjakZw7jzFc/9h0xi1VC8JVvu3Doe');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `gmail` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL
+  `password` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `candidature`
---
-ALTER TABLE `candidature`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `tuteur`
---
-ALTER TABLE `tuteur`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `candidature`
---
-ALTER TABLE `candidature`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT pour la table `tuteur`
---
-ALTER TABLE `tuteur`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
